5fa4cae6aba01e320306f17b356b5d05a153d525
Muhammad Arslan <gery_maz_555@hotmail.com>
2024-09-02 09:19:40 +0500

    initial commit

---
+++ app/src/main/java/com/mhdarslan/bigcart/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mhdarslan.bigcart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
